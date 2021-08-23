f2c023a9073d0605b9e75c70bdb0c456f8e7adfb
wuhaiqiang <18820185804@163.com>
2021-08-23 17:35:17 +0800

    新增io装饰测试代码

--- 《Head First设计模式》学习/Abc_DesignPattern_Demo/src/main/java/com/whq/designpattern/decorator/Test.java
+++ 《Head First设计模式》学习/Abc_DesignPattern_Demo/src/main/java/com/whq/designpattern/decorator/Test.java
@@ -3,6 +3,11 @@ package com.whq.designpattern.decorator;
 import com.whq.designpattern.decorator.beverage.DarkRoast;
 import com.whq.designpattern.decorator.decorator.MilkCondimentDecorator;
 import com.whq.designpattern.decorator.decorator.MochaCondimentDecorator;
+import com.whq.designpattern.decorator.other.LowerCaseInputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @Description: 测试类
@@ -10,12 +15,34 @@ import com.whq.designpattern.decorator.decorator.MochaCondimentDecorator;
  * @Date: 2021/8/23 16:25
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //testDecorator();
+        testIoDecorator();
+    }
+
+    public static void testDecorator() {
         AbstractBeverage darkRoast = new DarkRoast();
-        System.out.println("描述：" + darkRoast.getDescription() + "; 价格："+ darkRoast.cost());
+        System.out.println("描述：" + darkRoast.getDescription() + "; 价格：" + darkRoast.cost());
         darkRoast = new MilkCondimentDecorator(darkRoast);
-        System.out.println("描述：" + darkRoast.getDescription() + "; 价格："+ darkRoast.cost());
+        System.out.println("描述：" + darkRoast.getDescription() + "; 价格：" + darkRoast.cost());
         darkRoast = new MochaCondimentDecorator(darkRoast);
-        System.out.println("描述：" + darkRoast.getDescription() + "; 价格："+ darkRoast.cost());
+        System.out.println("描述：" + darkRoast.getDescription() + "; 价格：" + darkRoast.cost());
+    }
+
+    public static void testIoDecorator() throws IOException {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("ABCDEFG".getBytes());
+        InputStream in = new LowerCaseInputStream(inputStream);
+        int c;
+        while ((c = in.read()) > 0){
+            System.out.print((char)c);
+        }
+        in.close();
+
+        /*byte[] temp = new byte[8];
+        int result;
+        while ((result = in.read(temp, 0, temp.length)) != -1) {
+            System.out.println(new String(temp, "utf-8"));
+        }
+        in.close();*/
     }
 }
